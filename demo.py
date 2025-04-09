090c05657552b57172af203a7d92e86d843c0d0c
Takib Yeasar <mdtakibyeasar@gmail.com>
2025-04-09 20:37:09 +0600

    feat: connect with mongodb, create logger & exception

---
+++ demo.py
@@ -0,0 +1,11 @@
+# from src.logger import logging
+# from src.exception import USvisaException
+# import sys
+
+# logging.info("Welcome to our custom log...")
+
+# try:
+#     a=2/0
+# except Exception as e:
+#     raise USvisaException(e, sys)
+
